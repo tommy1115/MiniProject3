1114d6b502c6a3eb14fc1e13754ca6be7d00e8ef
tommy1115 <107578364+tommy1115@users.noreply.github.com>
2022-06-19 15:56:04 +0800

    Add files via upload

---
+++ 106062332_project3.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Point {
+    int x, y;
+	Point() : Point(0, 0) {}
+	Point(float x, float y) : x(x), y(y) {}
+	bool operator==(const Point& rhs) const {
+		return x == rhs.x && y == rhs.y;
+	}
+	bool operator!=(const Point& rhs) const {
+		return !operator==(rhs);
+	}
+	Point operator+(const Point& rhs) const {
+		return Point(x + rhs.x, y + rhs.y);
+	}
+	Point operator-(const Point& rhs) const {
+		return Point(x - rhs.x, y - rhs.y);
+	}
+};
+
+class gomoku{
+    public:
+        enum SPOT_STATE {
+            EMPTY = 0,
+            BLACK = 1,
+            WHITE = 2
+        };
+        int player;
+        const int SIZE = 15;
+        std::array<std::array<int, SIZE>, SIZE> board;
+        void read_board(std::ifstream& fin) {
+            fin >> player;
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    fin >> board[i][j];
+                }
+            }
+        }
+};
+
+
+
+
+int main(int, char** argv){
+    std::ifstream fin(argv[1]);
+    std::ofstream fout(argv[2]);
+    gomoku.read_board(fin);
+
+    fin.close();
+    fout.close();
+    return 0;
+}
